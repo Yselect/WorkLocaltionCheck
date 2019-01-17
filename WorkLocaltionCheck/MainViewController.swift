972d850d544886ecf94f08c39fd7457abc1a3e7a
Ruengpakorn P <nick@mango.co.th>
2019-01-18 02:42:32 +0700

    Initial Commit

---
+++ WorkLocaltionCheck/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkLocaltionCheck
+//
+//  Created by nick ruengpakorn on 18/1/2562 BE.
+//  Copyright © 2562 nick ruengpakorn. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
